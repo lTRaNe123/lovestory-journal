0e39e38170a6158e7f861bf2b37f20b0a9401d1c
Dev <dev@lovestory.ru>
2026-03-29 11:32:04 +0000

    Improve mobile hero overlay readability

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require("express");
+const path = require("path");
 const app = express();
 app.use(express.static("."));
-app.get("*", (req, res) => res.sendFile("index.html", { root: "." }));
+app.get("/{*path}", (req, res) => res.sendFile(path.join(__dirname, "index.html")));
 app.listen(5000, "0.0.0.0", () => console.log("listening on 5000"));
